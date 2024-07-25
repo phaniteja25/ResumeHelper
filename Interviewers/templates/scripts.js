3e16c4ba5106beef607e5ab7ee090532470c0e52
phaniteja25 <tejavaishu2001@gmail.com>
2024-07-24 17:44:14 -0700

    added frontend pages

---
+++ Interviewers/templates/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('job-similarity').addEventListener('click', () => {
+    window.location.href = '\\Interviewers\\templates\\simlarity_checker.html';      
+
+});
+
+document.getElementById('interviewees').addEventListener('click', () => {
+    alert('For Interviewees clicked');
+});
---
+++ templates-landing_page/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('interviewers').addEventListener('click', () => {
+    window.location.href = '\\Interviewers\\templates\\interviewer.html';      
+                                                                                                   
+});
+
+document.getElementById('interviewees').addEventListener('click', () => {
+    window.location.href = '';
+});
